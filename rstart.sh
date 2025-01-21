e8ecba29a6ae51b0ee50bb1d970a6c0edb3dfae3
Aymeric Weinbach <397730+zecloud@users.noreply.github.com>
2025-01-21 17:07:16 +0100

    fix missing env

--- rstart.sh
+++ rstart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 # Créer le répertoire models s'il n'existe pas (il devrait être monté en tant que volume)
 #mkdir -p models
 if [ ! -d "models" ]; then
@@ -10,5 +11,8 @@ if [ ! -f "models/phi-4-q4.gguf" ]; then
 # Télécharger le fichier dans le répertoire models
     curl -L "https://huggingface.co/microsoft/phi-4-gguf/resolve/main/phi-4-q4.gguf?download=true" -o models/phi-4-q4.gguf
 fi
+
+source /root/.wasmedge/env
+
 
 wasmedge --dir .:. --nn-preload default:GGML:AUTO:models/phi-4-q4.gguf llama-api-server.wasm --prompt-template  phi-4-chat --ctx-size  16000 --model-name phi-3-mini --socket-addr 0.0.0.0:8080
